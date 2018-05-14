5f7d6498e8983e2863aa7450bd01dffedae86cc2
atomicMetal <harshlplv@gmail.com>
2018-05-14 11:34:54 +0530

    Formating of reources

--- app/src/main/java/com/metalsack/demouno/MainActivity.kt
+++ app/src/main/java/com/metalsack/demouno/MainActivity.kt
@@ -30,6 +30,7 @@ class MainActivity : AppCompatActivity() {
     var array = arrayOf("Melbourne", "Vienna", "Vancouver", "Toronto", "Calgary",
             "Adelaide", "Perth", "Auckland", "Helsinki", "Hamburg", "Munich", "New York",
             "Sydney", "Paris", "Cape Town", "Barcelona", "London", "Bangkok")
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
